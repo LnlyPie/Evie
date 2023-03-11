50c549b0d1d93beb1492a2770272675ce3db2628
Andus <30293380+AndusDEV@users.noreply.github.com>
2023-03-11 22:18:54 +0100

    Small settings changes
     - Removed fullscreen setting as it's not needed in Switch port
     - Changed default resolution in settings

--- scripts/misc/settings/Settings.gd
+++ scripts/misc/settings/Settings.gd
@@ -3,10 +3,9 @@ extends Node
 var cfg = ConfigFile.new()
 
 # Screen
-var screen_width = 1920
-var screen_height = 1080
+var screen_width = 1280
+var screen_height = 720
 var framerate_cap = 60
-var fullscreen = true
 
 # Audio
 var master_volume = 1 # ex. 0.5 = 50%
@@ -23,7 +22,6 @@ func save():
 	cfg.set_value("Screen", "width", screen_width)
 	cfg.set_value("Screen", "height", screen_height)
 	cfg.set_value("Screen", "framerate", framerate_cap)
-	cfg.set_value("Screen", "fullscreen", fullscreen)
 	# Audio
 	cfg.set_value("Audio", "master", master_volume)
 	# Gameplay
@@ -39,7 +37,6 @@ func load():
 	screen_width = cfg.get_value("Screen", "width")
 	screen_height = cfg.get_value("Screen", "height")
 	framerate_cap = cfg.get_value("Screen", "framerate")
-	fullscreen = cfg.get_value("Screen", "fullscreen")
 	# Audio
 	master_volume = cfg.get_value("Audio", "master")
 	# Gameplay
@@ -53,7 +50,6 @@ func apply():
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT,\
 	 SceneTree.STRETCH_ASPECT_EXPAND, Vector2(screen_width, screen_height))
 	Engine.set_target_fps(framerate_cap)
-	OS.window_fullscreen = fullscreen
 	# Audio
 	
 	# Gameplay
