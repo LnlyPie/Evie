c231c834467e49f3f281437276cad80582712778
Andus <30293380+AndusDEV@users.noreply.github.com>
2023-04-12 17:15:09 +0200

    Revert

--- scripts/misc/achievements/Achievements.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-# To do
--- scripts/misc/other/Notification.gd
+++ scripts/misc/other/Notification.gd
@@ -6,6 +6,9 @@ var notification_playing = false
 func send_notification(title: String, desc: String, icon: String):
 	$Panel/NotTitle.text = title
 	$Panel/NotDesc.text = desc
+	if icon == "gj":
+		$Panel/Sprite.scale = Vector2(6, 6)
+		$Panel/Sprite.texture = load("res://sprites/icons/misc/gj.png")
 	if icon == "quest":
 		$Panel/Sprite.scale = Vector2(3.5, 3.5)
 		$Panel/Sprite.texture = load("res://sprites/icons/misc/quest.png")
