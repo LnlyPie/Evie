026e5817404bda9c7112b5d2a36a101b1c0f7eaf
Andus <30293380+AndusDEV@users.noreply.github.com>
2023-04-25 22:11:54 +0200

    Notification Queue Working
    Notifications script wasn't the issue. The SceneTransition script was "loading" the scenes twice. The first one was a check if the scene exists.

--- scripts/misc/other/SceneTransition.gd
+++ scripts/misc/other/SceneTransition.gd
@@ -3,8 +3,5 @@ extends CanvasLayer
 func change_scene(target: String) -> void:
 	$AnimationPlayer.play("Dissolve")
 	yield($AnimationPlayer, "animation_finished")
-	if get_tree().change_scene(target) != OK:
-		print("Could not find scene: " + target)
-	else:
-		get_tree().change_scene(target)
+	get_tree().change_scene(target)
 	$AnimationPlayer.play_backwards("Dissolve")
