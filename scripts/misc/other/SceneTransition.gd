f2642ad9596d9f80ee1c41086870377237f615dd
Andus <30293380+AndusDEV@users.noreply.github.com>
2023-04-30 20:40:48 +0200

    Squashed commit of the following:
    commit b4f90267c086effc9c74b351af3dbbde672bdf6a
    Author: Andus <30293380+AndusDEV@users.noreply.github.com>
    Date:   Sun Apr 30 20:36:09 2023 +0200
    
        A few changes before 0.4.0 release
    
        - New Scene Transition: Scale in/out
        - New Player Animation
    
    commit c1da53eec5002cb1ae6cac7ae38356c97b8732a4
    Author: Andus <30293380+AndusDEV@users.noreply.github.com>
    Date:   Tue Apr 25 22:19:07 2023 +0200
    
        Removed test Notifications
    
        Just to be sure they won't go into final game (for some reason)
    
    commit 026e5817404bda9c7112b5d2a36a101b1c0f7eaf
    Author: Andus <30293380+AndusDEV@users.noreply.github.com>
    Date:   Tue Apr 25 22:11:54 2023 +0200
    
        Notification Queue Working
    
        Notifications script wasn't the issue. The SceneTransition script was "loading" the scenes twice. The first one was a check if the scene exists.
    
    commit d040ad1e97296679613e2e0cd49ca22dc73dee04
    Author: Andus <30293380+AndusDEV@users.noreply.github.com>
    Date:   Mon Apr 24 21:35:36 2023 +0200
    
        Revert "Merge branch 'main' into main"
    
        This reverts commit e3cd44b4bf9192d3c0b99142d5123b88f2c4e3b5, reversing
        changes made to 66635b5756470a31a4e962101d234bd5236a1a50.

--- scripts/misc/other/SceneTransition.gd
+++ scripts/misc/other/SceneTransition.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
 
-func change_scene(target: String) -> void:
-	$AnimationPlayer.play("Dissolve")
-	yield($AnimationPlayer, "animation_finished")
-	if get_tree().change_scene(target) != OK:
-		print("Could not find scene: " + target)
-	else:
+func change_scene(target: String, type: String = "Dissolve") -> void:
+	# Dissolve/Fade Away
+	if type == "Dissolve":
+		$AnimationPlayer.play("Dissolve")
+		yield($AnimationPlayer, "animation_finished")
 		get_tree().change_scene(target)
-	$AnimationPlayer.play_backwards("Dissolve")
+		$AnimationPlayer.play_backwards("Dissolve")
+	# Scale in/out
+	if type == "Scale":
+		$AnimationPlayer.play("Scale")
+		yield($AnimationPlayer, "animation_finished")
+		get_tree().change_scene(target)
+		$AnimationPlayer.play_backwards("Scale")
