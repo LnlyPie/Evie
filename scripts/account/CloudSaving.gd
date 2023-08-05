0c37399411aed2717243c2e9db7b7a91ac652434
Andus <30293380+AndusDEV@users.noreply.github.com>
2023-08-05 14:38:02 +0200

    Cloud saving, achievements check seperated from others

--- scripts/account/CloudSaving.gd
+++ scripts/account/CloudSaving.gd
@@ -8,11 +8,12 @@ func upload_saves():
 		var dat_file = Utils.savesFolder + save_folder + "/save.dat"
 		var cfg_file = Utils.savesFolder + save_folder + "/save.cfg"
 		var ach_file = Utils.savesFolder + save_folder + "/achievements.json"
-		
-		if File.new().file_exists(dat_file) and File.new().file_exists(cfg_file) and File.new().file_exists(ach_file):
+
+		if File.new().file_exists(dat_file) and File.new().file_exists(cfg_file):
 			var save_dat = yield(Firebase.Storage.ref("users/" + Firebase.Auth.auth["localid"] + "/games/evie/saves/" + save_folder + "/save.dat").put_file(dat_file), "task_finished")
-			var save_cfg = Firebase.Storage.ref("users/" + Firebase.Auth.auth["localid"] + "/games/evie/saves/" + save_folder + "/save.cfg").put_file(cfg_file)
-			var save_ach = Firebase.Storage.ref("users/" + Firebase.Auth.auth["localid"] + "/games/evie/saves/" + save_folder + "/achievements.json").put_file(ach_file)
+			var save_cfg = yield(Firebase.Storage.ref("users/" + Firebase.Auth.auth["localid"] + "/games/evie/saves/" + save_folder + "/save.cfg").put_file(cfg_file), "task_finished")
+			if File.new().file_exists(ach_file):
+				var save_ach = yield(Firebase.Storage.ref("users/" + Firebase.Auth.auth["localid"] + "/games/evie/saves/" + save_folder + "/achievements.json").put_file(ach_file), "task_finished")
 		else:
 			print("Save " + str(save_index) + " doesn't exist! It will not be saved to the cloud.")
 
